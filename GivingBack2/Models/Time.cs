14ff787f62233615e02fad02f0e5bd6511e05cd7
Abhishek Kumar (CRM BLR) <abhk@microsoft.com>
2017-07-25 18:49:09 +0530

    merge conflicts resolvef

---
+++ GivingBack2/Models/Time.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GivingBack2.Models
+{
+	public class Time
+	{
+		public static List<string> TimeList = new List<string>();
+
+		static Time()
+		{
+			TimeList.Add("12 AM");
+
+			for (int i = 1; i < 12; i++)
+				TimeList.Add(i.ToString()+" AM");
+
+			TimeList.Add("12 PM");
+
+			for (int i = 1; i < 12; i++)
+				TimeList.Add(i.ToString() + " PM");
+
+		}
+
+		public int SelectedTimeIndex { get; set; }
+		public string SelectedTime
+		{
+			get
+			{
+				return TimeList[SelectedTimeIndex];
+			}
+		}
+	}
+}
