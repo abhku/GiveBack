41dd6a93dcbdd5be1d3f83f191ef39f9cebff999
Karthik Subramanian <Karthik.Subramanian@microsoft.com>
2017-07-26 06:20:33 +0530

    Files Addition

---
+++ GivingBack2/Models/EntityManager/RequirementManager.cs
@@ -0,0 +1,43 @@
+﻿using GivingBack2.Models.DB;
+using GivingBack2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GivingBack2.Models.EntityManager
+{
+	public class RequirementManager
+	{
+		public MappedRequirementViewModel TimeRequirementMapping(SpecifyParametersViewModel specifyParametersViewModel)
+		{
+			MappedRequirementViewModel mappedRequirementViewModel = new MappedRequirementViewModel();
+
+			// Business Logic here
+
+
+			return mappedRequirementViewModel;
+		}
+
+		public MappedRequirementViewModel ProductRequirementMapping(SpecifyParametersViewModel specifyParametersViewModel)
+		{
+			MappedRequirementViewModel mappedRequirementViewModel = new MappedRequirementViewModel();
+
+			// Business Logic here
+
+
+			return mappedRequirementViewModel;
+		}
+
+		public MappedRequirementViewModel MoneyRequirementMapping(SpecifyParametersViewModel specifyParametersViewModel)
+		{
+			MappedRequirementViewModel mappedRequirementViewModel = new MappedRequirementViewModel();
+
+			// Business Logic here
+			GiveBackDBEntities db = new GiveBackDBEntities();
+
+
+			return mappedRequirementViewModel;
+		}
+	}
+}
